18a2b482698a08442a2cd830c21cb64462048e63
akhilmeleppura <akhilmeleppura@gmail.com>
2026-08-19 19:17:14 +0530

    fix: include Boxicons stylesheet for crisp icon rendering across all pages

--- resources/views/layouts/sections/styles.blade.php
+++ resources/views/layouts/sections/styles.blade.php
@@ -8,6 +8,7 @@
 
 <!-- Fonts Icons -->
 @vite(['resources/assets/vendor/fonts/iconify/iconify.css'])
+<link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
 
 <!-- BEGIN: Vendor CSS-->
 @if ($configData['hasCustomizer'])
